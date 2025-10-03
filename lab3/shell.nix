927f4e65b5ebe5cd0e356a47041a7ecbdb448bf6
Kirill <kremizov3@gmail.com>
2025-10-03 14:53:49 +0300

    fixup! feat(.github/workflows/blank.yml): Pipline for lab3

--- lab3/shell.nix
+++ lab3/shell.nix
@@ -3,14 +3,19 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     python3
-    python3Packages.numpy
-    python3Packages.opencv4
     python3Packages.matplotlib
-    python3Packages.scipy
+
   ];
 
   shellHook = ''
-    echo "Среда загружена. Доступны: python, numpy, opencv, matplotlib"
-    echo "Для выполнения задания запустите: python task.py"
+    if [ ! -d ".venv" ]; then
+      python -m venv .venv
+    fi
+
+    source .venv/bin/activate
+
+    if [ -f "dependencies.txt" ]; then
+      pip install -r dependencies.txt
+    fi
   '';
 }
